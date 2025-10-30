4104d5cdada7b6b96f3d623fac3f62681b152050
jamesotto852 <jamesotto852@gmail.com>
2025-10-30 18:35:57 +0000

    Fix broken package documentation

---
+++ R/tldrDocs-package.R
@@ -0,0 +1,15 @@
+#' tldrDocs: tldr-style documentation for base R packages
+#'
+#' A collection of tldr-style documentation for commonly-used objects exported in
+#' the base distribution of R.
+#'
+#' @seealso
+#'
+#' Useful links:
+#' * \url{https://github.com/jamesotto852/tldrDocs/}
+#' * \url{https://github.com/jamesotto852/tldrPages/}
+#'
+#' @docType package
+#' @name tldrDocs
+#' @aliases tldrDocs package-tldrDocs
+NULL
